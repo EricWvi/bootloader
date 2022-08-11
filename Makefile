391793615d9bdd9c16c83bcd34ffbf4e57a3061c
EricWvi <eric2000wvi@gmail.com>
2022-08-11 18:57:11 +0800

    qemu test

--- Makefile
+++ Makefile
@@ -6,4 +6,11 @@ build_path := target/$(target)/$(MODE)
 ESP := $(build_path)/esp
 OVMF := OVMF.fd
 
-
+# -serial mon:stdio + -nographic
+run:
+	qemu-system-x86_64 \
+	-drive if=pflash,format=raw,readonly,file=OVMF.fd \
+	-drive format=raw,file=fat:rw:target/x86_64-unknown-uefi/debug \
+	-m 4G \
+	-device isa-debug-exit \
+	-net none
--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,15 @@
 #![no_std]
 #![feature(abi_efiapi)]
 
+extern crate alloc;
+
+use alloc::vec::Vec;
+use core::ops::DerefMut;
 use log::info;
 use uefi::prelude::*;
+use uefi::proto::media::file::{File, FileAttribute, FileHandle, FileMode, RegularFile};
+use uefi::proto::media::fs::SimpleFileSystem;
+use uefi::CStr16;
 
 #[entry]
 fn main(_handle: Handle, mut system_table: SystemTable<Boot>) -> Status {
@@ -18,5 +25,33 @@ fn main(_handle: Handle, mut system_table: SystemTable<Boot>) -> Status {
     // You must arrange for the `disable` method to be called or for this logger
     // to be otherwise discarded before boot services are exited.
 
+    // log 截图放到 wiki 里面
     Status::SUCCESS
+}
+
+// readelf
+
+fn open_file(bs: &BootServices, path: &str) -> RegularFile {
+    info!("opening file: {}", path);
+    // TODO: 测试不加 fs 报错情况
+    let fs = bs
+        .locate_protocol::<SimpleFileSystem>()
+        .expect("failed to get SimpleFileSystem");
+    let fs = unsafe { &mut *fs.get() };
+    let mut root = match fs.open_volume() {
+        Err(e) => panic!("{:?}", e),
+        Ok(dir) => dir,
+    };
+    // let mut buf = Vec::with_capacity(path.len());
+    // TODO: 查看报错
+    let mut buf = [0; 2];
+    let path_cstr = match CStr16::from_str_with_buf(path, &mut buf) {
+        Err(e) => panic!("{:?}", e),
+        Ok(str) => str,
+    };
+    // TODO: 写一个不存在的文件查看报错
+    match root.open(path_cstr, FileMode::Read, FileAttribute::empty()) {
+        Ok(handle) => unsafe { RegularFile::new(handle) },
+        Err(e) => panic!("{:?}", e),
+    }
 }
